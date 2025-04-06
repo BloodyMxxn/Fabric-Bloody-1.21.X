d6f1b000f9dfe088caa1c5d38ccd070c6a462eae
BloodyMxxn <Oma83ytgaming@gmail.com>
2025-04-06 13:44:38 -0400

    Initial commit

---
+++ src/main/java/net/bloodymxxn/bloodymod/BloodyMod.java
@@ -0,0 +1,16 @@
+package net.bloodymxxn.bloodymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BloodyMod implements ModInitializer {
+	public static final String MOD_ID = "bloodymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bloodymxxn/bloodymod/BloodyModClient.java
@@ -0,0 +1,10 @@
+package net.bloodymxxn.bloodymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BloodyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
